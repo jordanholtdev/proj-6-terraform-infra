5b926e099a8df0195f82dacd1b6548f30e53baa4
Jordan <jordan.su@protonmail.com>
2023-06-25 18:26:08 -0400

    add cognito resource

--- variables.tf
+++ variables.tf
@@ -1,17 +1,6 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
+variable "user_pool_name" {
+  description = "Value of the Name for the Cognito User Pool"
   type        = string
-  default     = "Project6AppServerInstance"
+  default     = "Project6AppUserPool"
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to launch"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "ami" {
-  description = "AMI to use for the EC2 instance"
-  type        = string
-  default     = "ami-026ebd4cfe2c043b2"
-}
