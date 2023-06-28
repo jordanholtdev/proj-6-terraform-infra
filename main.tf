ff1d281cf2d5cf794bc4780803cbe4302b465863
Jordan <jordan.su@protonmail.com>
2023-06-28 16:41:57 -0400

    CU-860r79n9t[in progress]

--- main.tf
+++ main.tf
@@ -42,3 +42,57 @@ resource "aws_cognito_user_pool_client" "webapp" {
 
   user_pool_id = aws_cognito_user_pool.Project6AppUserPool.id
 }
+
+
+resource "aws_s3_bucket" "images" {
+  bucket = "images-bucket-project6"
+
+  tags = {
+    Name        = "Project 6 Images Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "images_bucket_cors_conf" {
+  bucket = aws_s3_bucket.images.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "images_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.images.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "images_bucket_public_access_block" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "images_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.images_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.images_bucket_public_access_block,
+  ]
+
+  bucket = aws_s3_bucket.images.id
+  acl    = "public-read"
+}
--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,7 @@ output "user_pool_id" {
   value = aws_cognito_user_pool.Project6AppUserPool.id
 }
 
-
+output "aws_s3_bucket_id" {
+  description = "The ID of the S3 Bucket"
+  value = aws_s3_bucket.images.id
+}
